36827ba2d10f36e39459745ce6f24b1faf239958
Noah Finer <nfiner0149@gmail.com>
2016-02-26 15:45:28 -0700

    Make the colors more fairview-ey, fix fonts, add animations, fix #1, #2.

---
+++ script.js
@@ -0,0 +1,7 @@
+// $(window).on("scroll", function() {
+//   $("#landing").css("background-position-y", $(window).scrollTop()+"px");
+// });
+//
+// $(document).ready(function() {
+//   $("#landing").css("background-position-y", "0px");
+// });
