f14036bbf4d24a11d7817f8cf0793b81076d3324
Noah Finer <nfiner0149@gmail.com>
2016-02-26 15:48:09 -0700

    Add a parallax. Fix #2

--- script.js
+++
@@ -1,7 +0,0 @@
-// $(window).on("scroll", function() {
-//   $("#landing").css("background-position-y", $(window).scrollTop()+"px");
-// });
-//
-// $(document).ready(function() {
-//   $("#landing").css("background-position-y", "0px");
-// });
